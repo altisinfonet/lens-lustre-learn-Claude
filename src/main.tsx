3d211301239129f7f3eaaefcbf8b0ca02a29b0f7
Altis Infonet Private limited <mail@altisinfonet.com>
2026-08-01 11:46:43 +0530

    Bump __APP_BUILD to 2026-08-01-7

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import "./index.css";
 // doesn't update. It is a window side-effect (NOT a bare export, which gets
 // tree-shaken away) so it survives into the bundle and forces a new hashed
 // entry filename, busting year-cached immutable copies of the old bundle.
-(window as any).__APP_BUILD = "2026-08-01-6";
+(window as any).__APP_BUILD = "2026-08-01-7";
 
 startNetworkTrace(8000);
 
